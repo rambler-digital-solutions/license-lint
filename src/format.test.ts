bfe45c1f9fd08f4d0cf26b8737d1b14f5e943814
Andrey Polischuk <me@andrepolischuk.com>
2023-01-13 02:14:54 +0300

    test: add format tests

---
+++ src/format.test.ts
@@ -0,0 +1,40 @@
+import chalk from 'chalk'
+import {format} from './format'
+
+const results = [
+  {
+    name: 'foo@1.0.0',
+    licenses: 'MIT'
+  },
+  {
+    name: 'bar@1.0.0',
+    licenses: 'ISC'
+  },
+  {
+    name: 'baz@1.0.0',
+    licenses: 'GPL'
+  }
+]
+
+test('format success results', () => {
+  const output = format(results)
+  expect(output).toContain(`foo@1.0.0  ${chalk.dim('MIT')}`)
+  expect(output).toContain(`bar@1.0.0  ${chalk.dim('ISC')}`)
+  expect(output).toContain(`baz@1.0.0  ${chalk.dim('GPL')}`)
+})
+
+test('format error results', () => {
+  const resultsWithError = [
+    ...results.slice(0, 2),
+    {
+      name: 'baz@1.0.0',
+      licenses: 'GPL',
+      error: 'GPL is denied by `deny` list'
+    }
+  ]
+  const output = format(resultsWithError)
+  expect(output).toContain(`foo@1.0.0  ${chalk.dim('MIT')}`)
+  expect(output).toContain(`bar@1.0.0  ${chalk.dim('ISC')}`)
+  expect(output).toContain(`1 error`)
+  expect(output).toContain(`baz@1.0.0  ${chalk.dim('GPL')}  GPL is denied`)
+})
